77357cd98520d75fbfa7e5f2d8e254a3a94aea29
HeZitong <hezitong@HeZitongdeMacBook-Pro.local>
2014-11-06 20:54:14 +0800

    Initial Commit

---
+++ myWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myWeibo
+//
+//  Created by HeZitong on 14/11/6.
+//  Copyright (c) 2014年 HeZitong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
